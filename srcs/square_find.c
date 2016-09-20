3fafa126518e731ac39e6598535418a01b34dfa7
Evaldo NETO <eneto@e2r3p16.42.fr>
2016-09-20 16:23:37 +0200

    broke the square_find functions into 2 to adapt to the norm and fixed the algorithm

--- srcs/square_find.c
+++ srcs/square_find.c
@@ -1,87 +1,100 @@
-/*
-* Square matching algorithm!
-* Will return the position (top-down / left-right) of the biggest square found.
-* The return value will be a string of two integers.
-* It will receive the entire matrix of integers (0 and 1) as an input.
-*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   square_find.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/20 14:53:46 by eneto             #+#    #+#             */
+/*   Updated: 2016/09/20 16:13:41 by eneto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../includes/bsq.h"
 #include <stdio.h>
 
-
-int     *square_find(int **user_input, int size_y, int size_x)
+int		min_xyz(int x, int y, int z)
 {
-    int     i;
-    int     j;
-    int     *pos;
-    int     biggest;
-    int     **matched;
-    
-    pos = (int *)malloc(sizeof(int) * 3);
-    i = 0;
-    j = 1; // as we will start scaning from the second column
-    matched = (int **)malloc(sizeof(int *) * size_y);
-    
-    // Here we will transcribe the values recived to the first
-    // line and column of the "matched"array
-    while (i < size_y)
-    {
-        matched[i] = (int *)malloc(sizeof(int) * size_x);
-        i++;
-    }
-    i = 0;
-    
-    while (i < size_x)
-    {
-        matched[0][i] = user_input[0][i];
-        i++;
-    }
-    i = 0; // reinitialize the counter so we can user afterwards
-    while (i < size_y)
-    {
-        matched[i][0] = user_input[i][0];
-        i++;
-    }
-    // Will do the scan and create the array with the answers
-    i = 1; // as we will start scaning from the second line
-    
-    while (i < size_y)
-    {
-    
-        while (j < size_x)
-        {
-            if (user_input[i][j] == 0) //if we find an obstacle we just go verify the next one
-                matched[i][j] = 0;
-            else if (matched[i - 1][j - 1] <= matched[i][j - 1] && matched[i - 1][j - 1] <= matched[i - 1][j] )
-                matched[i][j] = matched[i - 1][j - 1] + 1;
-            else
-                matched[i][j] = user_input[i][j];
-            j++;
-        }
-        j = 1;
-        i++;
-    }
-    print_array(matched, size_y, size_x);
-    i = size_y - 1;
-    j = size_x - 1;
-    //here we will scan the resultung "matched"for the biggest result
-    // from end to beggining. Like this the first to appear will stay
-    biggest = 0;
-    while (i >= 0)
-    {
-        while (j >= 0)
-        {
-            if (matched[i][j] >= biggest)
-            {
-                biggest = matched[i][j];
-                pos[0] = j - (biggest - 1);
-                pos[1] = i - (biggest - 1);
-            }
-            j--;
-        }
-        j = size_x;
-        i--;
-    }
-    pos[2] = biggest;
-    return (pos);
+	if (x <= y && x <= z)
+		return (x);
+	else if (y <= x && y <= z)
+		return (y);
+	else if (z <= x && z <= y)
+		return (z);
+	return (0);
+}
+
+int		**init_square(int **user_input, int **matched, int size_y, int size_x)
+{
+	int i;
+
+	i = -1;
+	matched = (int **)malloc(sizeof(int *) * size_y);
+	while (i++ < size_y)
+		matched[i] = (int *)malloc(sizeof(int) * size_x);
+	i = -1;
+	while (i++ < size_x)
+		matched[0][i] = user_input[0][i];
+	i = 0;
+	while (i < size_y)
+	{
+		matched[i][0] = user_input[i][0];
+		i++;
+	}
+	return (matched);
+}
+
+int		**make_square(int **user_input, int size_y, int size_x)
+{
+	int		i;
+	int		j;
+	int		**matched;
+
+	matched = init_square(user_input, matched, size_y, size_x);
+	j = 0;
+	i = 1;
+	while (i < size_y)
+	{
+		while (j < size_x)
+		{
+			if (user_input[i][j] == 0)
+				matched[i][j] = 0;
+			else
+				matched[i][j] = min_xyz(matched[i - 1][j - 1],
+					matched[i][j - 1], matched[i - 1][j]) + 1;
+			j++;
+		}
+		j = 1;
+		i++;
+	}
+	print_array(matched, size_y, size_x);
+	return (matched);
+}
+
+int		*square_find(int **matched, int size_y, int size_x, int biggest)
+{
+	int		i;
+	int		j;
+	int		*pos;
+
+	pos = (int *)malloc(sizeof(int) * 3);
+	i = size_y - 1;
+	j = size_x - 1;
+	while (i >= 0)
+	{
+		while (j >= 0)
+		{
+			if (matched[i][j] >= biggest)
+			{
+				biggest = matched[i][j];
+				pos[0] = j - (biggest - 1);
+				pos[1] = i - (biggest - 1);
+			}
+			j--;
+		}
+		j = size_x;
+		i--;
+	}
+	pos[2] = biggest;
+	return (pos);
 }
