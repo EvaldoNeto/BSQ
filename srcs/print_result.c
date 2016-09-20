76cc824bbc5bd00a9bff697226901c1096acf09c
Evaldo NETO <eneto@e2r3p16.42.fr>
2016-09-20 21:58:27 +0200

    fixed norm

--- srcs/print_result.c
+++ srcs/print_result.c
@@ -6,7 +6,7 @@
 /*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 14:31:22 by eneto             #+#    #+#             */
-/*   Updated: 2016/09/20 17:27:56 by eneto            ###   ########.fr       */
+/*   Updated: 2016/09/20 21:41:43 by eneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	print_result(int **matched, int *size, int *pos, char *c)
 	j = 0;
 	while (i < size[0])
 	{
-		while (j < size[1 ])
+		while (j < size[1])
 		{
-			if ((i >= pos[1] && i < pos[1] + pos[2]) 
+			if ((i >= pos[1] && i < pos[1] + pos[2])
 				&& (j >= pos[0] && j < pos[0] + pos[2]))
 				ft_putchar('x');
 			else if (matched[i][j] == 0)
--- srcs/read_header.c
+++ srcs/read_header.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_header.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/20 21:42:15 by eneto             #+#    #+#             */
+/*   Updated: 2016/09/20 21:43:34 by eneto            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/bsq.h"
 
-char    *read_header(char *str)
+char	*read_header(char *str)
 {
-	char    *charact;
+	char	*charact;
 	int		i;
-	
+
 	i = 0;
 	charact = (char*)malloc(sizeof(char) * 3);
 	while (str[i] >= '0' && str[i] <= '9')
