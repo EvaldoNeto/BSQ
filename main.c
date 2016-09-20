813a23815024ba445a667187f5350feb2d030df2
Evaldo NETO <eneto@e2r3p16.42.fr>
2016-09-20 22:13:28 +0200

    still to fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/19 15:06:21 by eneto             #+#    #+#             */
-/*   Updated: 2016/09/20 21:32:00 by eneto            ###   ########.fr       */
+/*   Updated: 2016/09/20 22:08:16 by eneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,9 @@ int		main(int argc, char **argv)
 			str = file_to_string(argv[i], str);
 			size = measure_size(str);
 			charact = read_header(str);
-			pos = square_find(make_square(get_array(str, size, 0, 0, charact),
+			pos = square_find(make_square(get_array(str, size, 0, charact),
 										size[0], size[1]), size[0], size[1], 0);
-			print_result(make_square(get_array(str, size, 0, 0, charact), size[0],
+			print_result(make_square(get_array(str, size, 0, charact), size[0],
 									size[1]), size, pos, charact);
 			i++;
 			ft_putstr("\n");
