e1a60629c767f120f8e69ddbc205dcd554c6f478
Evaldo NETO <eneto@e2r3p18.42.fr>
2016-09-21 17:43:44 +0200

    fast and furious

--- srcs/files.c
+++ srcs/files.c
@@ -6,33 +6,34 @@
 /*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/19 16:02:16 by eneto             #+#    #+#             */
-/*   Updated: 2016/09/21 01:05:33 by eneto            ###   ########.fr       */
+/*   Updated: 2016/09/21 17:43:13 by eneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 #include "../includes/bsq.h"
+#include <stdio.h>
 
 char	*file_to_string(char *name_file, char *str)
 {
-	char	c;
+	char	c[4000];
 	int		i;
 	int		file;
+	int		tmp;
 
+	tmp = 0;
 	if ((file = open(name_file, O_RDONLY)) == -1)
 	{
 		ft_putstr("No such file or directory\n");
 		return (0);
 	}
 	i = 0;
-	while (read(file, &c, 1))
+	while ((tmp = read(file, c, 4001)))
 	{
-		str = ft_realloc(str, 2);
-		str[i] = c;
-		i++;
+		c[tmp] = '\0';
+		str = ft_join(str, c);
 	}
-	str[i] = '\0';
 	close(file);
 	return (str);
 }
